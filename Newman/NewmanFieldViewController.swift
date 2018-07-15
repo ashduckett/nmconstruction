2cec27418c8610247043c90121238997c6224198
Ash Duckett <ash@ashs-imac.home>
2018-07-15 23:00:04 +0100

    Some constraint issues but forms are now dynamic

--- Newman/NewmanFieldViewController.swift
+++ Newman/NewmanFieldViewController.swift
@@ -10,14 +10,51 @@ import UIKit
 
 class NewmanFormViewController: UIViewController {
 
+
     @IBOutlet weak var formContainer: UIStackView!
     
     // var waterProofingForm: [NewmanField]?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-      //  setupWaterProofingForm()
+        print("View did load")
+        setupWaterProofingForm()
         
+    }
+    
+    func removeAllItemsFromStack() {
+        let viewsToRemove = formContainer.arrangedSubviews
+        
+        for viewToRemove in viewsToRemove {
+            formContainer.removeArrangedSubview(viewToRemove)
+        }
+    }
+    
+    @IBAction func structuralButtonPressed(_ sender: Any) {
+        removeAllItemsFromStack()
+        setupStructuralForm()
+    }
+    @IBAction func stoneRepairButtonPressed(_ sender: Any) {
+        removeAllItemsFromStack()
+        setupStoneRepairForm()
+    }
+    
+    @IBAction func concreteRepairButtonPressed(_ sender: Any) {
+        removeAllItemsFromStack()
+        setupConcreteRepairForm()
+    }
+    @IBAction func brickRepairButtonPressed(_ sender: Any) {
+        removeAllItemsFromStack()
+        setupBrickRepairForm()
+    
+    }
+    @IBAction func waterProofingButtonPressed(_ sender: Any) {
+        removeAllItemsFromStack()
+        setupWaterProofingForm()
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print("end edit")
+        self.view.endEditing(true)
     }
     
     func addField(plusButtonTappedFor: NewmanField) {
@@ -26,6 +63,106 @@ class NewmanFormViewController: UIViewController {
         blankPair.delegate = self
         formContainer.insertArrangedSubview(blankPair, at: indexToAddControlTo)
         blankPair.setupViewHierarchy()
+    }
+    
+    func setupStoneRepairForm() {
+        let formOne = [
+            SingleCenteredFieldView(placeholders: [""], label: "QUOTE REQUISITION STONE REPAIR", textFieldCount: 0),
+            TextFieldPairView(placeholders: ["CLIENTS", "ADDRESS"], delegate: self),
+            TextFieldPairView(placeholders: ["SERVICE/s", "SPECIFICATION/s"], delegate: self),
+            MaterialField(placeholders: ["MATERIAL", "AMOUNT"], delegate: self, textFieldCount: 2),
+            FullLengthTextField(placeholders: ["ELEVATION/s"], delegate: self, textFieldCount: 1),
+            PictureField(placeholders: ["Insert Photo"], delegate: self, textFieldCount: 1, viewController: self),
+            SubstrateField(placeholders: ["SUBSTRATE TYPE", "COLOUR", "MORTAR COLOUR"], delegate: self, textFieldCount: 3),
+            PictureField(placeholders: ["Insert Drawings"], delegate: self, textFieldCount: 1, viewController: self),
+            FullLengthTextField(placeholders: ["DETAIL/s"], delegate: self, textFieldCount: 1),
+            SingleCenteredFieldView(placeholders: [""], label: "QUOTE NOTES", textFieldCount: 0),
+            ThreeTextFieldView(placeholders: ["SCAFFOLDING", "INSERT TYPE", "INSERT TYPE"], delegate: self, textFieldCount: 3),
+            TextFieldPairView(placeholders: ["GUARANTEE", "EXPECTED PRICE"], delegate: self),
+            TextFieldPairView(placeholders: ["", ""], delegate: self),
+            FullLengthTextField(placeholders: ["CLIENT TO PROVIDE"], delegate: self, textFieldCount: 1),
+            SingleCenteredFieldView(placeholders: [""], label: "APPROX TOTAL", textFieldCount: 0)
+        ]
+        
+        for formField in formOne {
+            formContainer.addArrangedSubview(formField)
+            formField.setupViewHierarchy()
+        }
+    }
+    
+    func setupConcreteRepairForm() {
+        let formOne = [
+            SingleCenteredFieldView(placeholders: [""], label: "QUOTE REQUISITION CONCRETE REPAIR", textFieldCount: 0),
+            TextFieldPairView(placeholders: ["CLIENTS", "ADDRESS"], delegate: self),
+            TextFieldPairView(placeholders: ["SERVICE/s", "SPECIFICATION/s"], delegate: self),
+            MaterialField(placeholders: ["MATERIAL", "AMOUNT"], delegate: self, textFieldCount: 2),
+            FullLengthTextField(placeholders: ["ELEVATION/s"], delegate: self, textFieldCount: 1),
+            PictureField(placeholders: ["Insert Photo"], delegate: self, textFieldCount: 1, viewController: self),
+            SubstrateField(placeholders: ["SUBSTRATE TYPE", "COLOUR", "MORTAR COLOUR"], delegate: self, textFieldCount: 3),
+            PictureField(placeholders: ["Insert Drawings"], delegate: self, textFieldCount: 1, viewController: self),
+            FullLengthTextField(placeholders: ["DETAIL/s"], delegate: self, textFieldCount: 1),
+            SingleCenteredFieldView(placeholders: [""], label: "QUOTE NOTES", textFieldCount: 0),
+            ThreeTextFieldView(placeholders: ["SCAFFOLDING", "INSERT TYPE", "INSERT TYPE"], delegate: self, textFieldCount: 3),
+            TextFieldPairView(placeholders: ["GUARANTEE", "EXPECTED PRICE"], delegate: self),
+            TextFieldPairView(placeholders: ["", ""], delegate: self),
+            FullLengthTextField(placeholders: ["CLIENT TO PROVIDE"], delegate: self, textFieldCount: 1),
+            SingleCenteredFieldView(placeholders: [""], label: "APPROX TOTAL", textFieldCount: 0)
+        ]
+        
+        for formField in formOne {
+            formContainer.addArrangedSubview(formField)
+            formField.setupViewHierarchy()
+        }
+    }
+    
+    func setupBrickRepairForm() {
+        let formOne = [
+            SingleCenteredFieldView(placeholders: [""], label: "QUOTE REQUISITION BRICK REPAIR", textFieldCount: 0),
+            TextFieldPairView(placeholders: ["CLIENTS", "ADDRESS"], delegate: self),
+            TextFieldPairView(placeholders: ["SERVICE/s", "SPECIFICATION/s"], delegate: self),
+            MaterialField(placeholders: ["MATERIAL", "AMOUNT"], delegate: self, textFieldCount: 2),
+            FullLengthTextField(placeholders: ["ELEVATION/s"], delegate: self, textFieldCount: 1),
+            PictureField(placeholders: ["Insert Photo"], delegate: self, textFieldCount: 1, viewController: self),
+            SubstrateField(placeholders: ["SUBSTRATE TYPE", "COLOUR", "MORTAR COLOUR"], delegate: self, textFieldCount: 3),
+            PictureField(placeholders: ["Insert Drawings"], delegate: self, textFieldCount: 1, viewController: self),
+            FullLengthTextField(placeholders: ["DETAIL/s"], delegate: self, textFieldCount: 1),
+            SingleCenteredFieldView(placeholders: [""], label: "QUOTE NOTES", textFieldCount: 0),
+            ThreeTextFieldView(placeholders: ["SCAFFOLDING", "INSERT TYPE", "INSERT TYPE"], delegate: self, textFieldCount: 3),
+            TextFieldPairView(placeholders: ["GUARANTEE", "EXPECTED PRICE"], delegate: self),
+            TextFieldPairView(placeholders: ["", ""], delegate: self),
+            FullLengthTextField(placeholders: ["CLIENT TO PROVIDE"], delegate: self, textFieldCount: 1),
+            SingleCenteredFieldView(placeholders: [""], label: "APPROX TOTAL", textFieldCount: 0)
+        ]
+        
+        for formField in formOne {
+            formContainer.addArrangedSubview(formField)
+            formField.setupViewHierarchy()
+        }
+    }
+    
+    func setupStructuralForm() {
+        let formOne = [
+            SingleCenteredFieldView(placeholders: [""], label: "QUOTE REQUISITION STRUCTURAL", textFieldCount: 0),
+            TextFieldPairView(placeholders: ["CLIENTS", "ADDRESS"], delegate: self),
+            TextFieldPairView(placeholders: ["SERVICE/s", "SPECIFICATION/s"], delegate: self),
+            MaterialField(placeholders: ["MATERIAL", "AMOUNT"], delegate: self, textFieldCount: 2),
+            FullLengthTextField(placeholders: ["ELEVATION/s"], delegate: self, textFieldCount: 1),
+            PictureField(placeholders: ["Insert Photo"], delegate: self, textFieldCount: 1, viewController: self),
+            SubstrateField(placeholders: ["SUBSTRATE TYPE", "COLOUR", "MORTAR COLOUR"], delegate: self, textFieldCount: 3),
+            PictureField(placeholders: ["Insert Drawings"], delegate: self, textFieldCount: 1, viewController: self),
+            FullLengthTextField(placeholders: ["DETAIL/s"], delegate: self, textFieldCount: 1),
+            SingleCenteredFieldView(placeholders: [""], label: "QUOTE NOTES", textFieldCount: 0),
+            ThreeTextFieldView(placeholders: ["SCAFFOLDING", "INSERT TYPE", "INSERT TYPE"], delegate: self, textFieldCount: 3),
+            TextFieldPairView(placeholders: ["GUARANTEE", "EXPECTED PRICE"], delegate: self),
+            TextFieldPairView(placeholders: ["", ""], delegate: self),
+            FullLengthTextField(placeholders: ["CLIENT TO PROVIDE"], delegate: self, textFieldCount: 1),
+            SingleCenteredFieldView(placeholders: [""], label: "APPROX TOTAL", textFieldCount: 0)
+        ]
+        
+        for formField in formOne {
+            formContainer.addArrangedSubview(formField)
+            formField.setupViewHierarchy()
+        }
     }
     
     func setupWaterProofingForm() {
