801b9eaa6ce9ca20bd531293b7b7ab50727971f1
Ash Duckett <ash@ashs-imac.home>
2018-07-15 21:22:53 +0100

    Basic layout without fields

---
+++ Newman/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Newman
+//
+//  Created by Ash Duckett on 15/07/2018.
+//  Copyright © 2018 Ash Duckett. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
